53462d1e98e79165a308bbebcff0a91dee2dfc26
jkelly101 <jenLkelly@gmail.com>
2021-03-20 14:19:32 -0400

    new connection string added

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "jkelly101",
-  database: "burgers_db"
-});
+let connection;
+if (process.env.JAWSDB_URL && process.env.JAWSDB_URL.length > 0) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "jkelly101",
+    database: "burgers_db",
+  });
+}
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
